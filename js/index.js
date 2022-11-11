5ebd083cf0a9769813b3ce336d5dd3e055126c4b
= <=>
2022-11-11 17:43:05 -0300

    Agenda que atualiza o mês no topo

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-var calendarioDias = document.getElementById("dias")
+var calendarDays = document.getElementById("dias")
+var calendarMonth = document.getElementById("mes")
 var data = new Date()
 var dayNow = data.getDate()
-// console.log(day)
-// for(let day = 1; day >= 31; day++){
-//     calendarioDias.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
-// }
-//Listar Dias do Mês
-for(let day = 1; day <= 31; day++){
-    
-    if (day == dayNow) {
-        calendarioDias.insertAdjacentHTML("beforeend",`<div class="dayNow">${day}</div>`)
+limite = 31
+
+meses = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let mes = meses[data.getMonth()]
+
+//Verificar qual o Mês e mudar o limite de dias
+mesesL = ["January","April","June","September","November"]
+for(let i = 0; i <= mesesL.length - 1; i++){
+    if (mes == mesesL[i]) {
+        limite = 30
     } else {
-        calendarioDias.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
+        limite = 28
+    }
+}
+
+//Coloca o Mes
+calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${mes}</h2>`)
+
+//Coloca os Dias do Mês
+for(let day = 1; day <= limite; day++){
+    if (day == dayNow) {
+        calendarDays.insertAdjacentHTML("beforeend",`<div class="dayNow">${day}</div>`)
+    } else {
+        calendarDays.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
     }
 }
