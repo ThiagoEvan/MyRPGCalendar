e8bbcefed25ab4632e268a508cfc76374ec99d0c
= <=>
2022-11-21 01:42:02 -0300

    Agenda basica quase funcional

--- js/index.js
+++ js/index.js
@@ -1,34 +1,32 @@
 var calendarDays = document.getElementById("dias")
 var calendarMonth = document.getElementById("mes")
-var d = new Date()
-var full_year = d.getFullYear()
-var month = d.getMonth() + 1
-var date = new Date(`${full_year} ${month} 01`)
+var date = new Date()
 var dayNow = date.getDate()
-limite = 31
 
-meses = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let mes = meses[d.getMonth()]
-
-//Verificar o dia que começas
+//Verificar o dia que começa e os dias do mês anterior
 week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"] //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sabado
-let w = date.toDateString().slice(0,3)
-for(let i = 0; i <= week.length - 1; i++){
-    if (w == week[i]) {
-        console.log(`O 1° dia do mês começa em ${w}`)
-    }
+let w = week[date.getDay()]
+var l = date.getDay()
+for(let i = 0; i <= l; i++){
+    date.setDate(-1)
+    let previous_day = date.getDate()
+    calendarDays.insertAdjacentHTML("afterbegin",`<div class="previous_day">${previous_day+1}</div>`)
 }
-//Verificar qual o Mês e mudar o limite de dias
-mesesL = ["January","April","June","September","November"]
-for(let i = 0; i <= mesesL.length - 1; i++){
-    if (mes == mesesL[i]) {
+
+//Verificar o limite de dias do Mês atual
+date = new Date()
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var thirty_months = ["January","April","June","September","November"]
+let month = months[date.getMonth()]
+var limite = 31
+for(let i = 0; i <= thirty_months.length - 1; i++){
+    if (month == thirty_months[i]) {
         limite = 30
     } else {
         limite = 28
     }
 }
-
-//Coloca o Mes
+//Coloca o Mês atual
 calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${mes}</h2>`)
 
 //Coloca os Dias do Mês
@@ -38,4 +36,11 @@ for(let day = 1; day <= limite; day++){
     } else {
         calendarDays.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
     }
+}
+
+//Verificar os proximos dias do proximo mês (testing...)
+var m = date.getMonth()
+date.setMonth(m+1)
+for(let i = l; i <= 3; i++){
+    calendarDays.insertAdjacentHTML("beforeend",`<div class="dayNow">${l}</div>`)
 }
