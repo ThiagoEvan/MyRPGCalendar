7af740cd6067fbe40f9bdad1d5e7fe650e5bd6c8
unknown <11vega@protonmail.com>
2022-11-22 20:51:50 -0300

    Adicionando função de tarefa

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var dayNow = date.getDate()
 week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"] //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sabado
 let w = week[date.getDay()]
 var l = date.getDay()
-for(let i = 0; i <= l; i++){
+for(let i = 0; i <= l -1 ; i++){
     date.setDate(-1)
     let previous_day = date.getDate()
     calendarDays.insertAdjacentHTML("afterbegin",`<div class="previous_day">${previous_day+1}</div>`)
@@ -27,7 +27,7 @@ for(let i = 0; i <= thirty_months.length - 1; i++){
     }
 }
 //Coloca o Mês atual
-calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${mes}</h2>`)
+calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${month}</h2>`)
 
 //Coloca os Dias do Mês
 for(let day = 1; day <= limite; day++){
@@ -39,8 +39,37 @@ for(let day = 1; day <= limite; day++){
 }
 
 //Verificar os proximos dias do proximo mês (testing...)
-var m = date.getMonth()
-date.setMonth(m+1)
-for(let i = l; i <= 3; i++){
-    calendarDays.insertAdjacentHTML("beforeend",`<div class="dayNow">${l}</div>`)
+date.setDate(limite+1)
+w = week[date.getDay()]
+for(let i = 0; i <= week.length - 1; i++){
+    if(w == week[i]){
+        limite = 7 - i
+    }
+    
 }
+for(let i = date.getDate(); i <= limite; i++){
+    calendarDays.insertAdjacentHTML("beforeend",`<div class="next_day">${i}</div>`)
+}
+
+//Mostrar tarefea
+calendarDays.addEventListener("click",function(){
+    document
+    .getElementById("modal")
+    .classList
+    .toggle("hide")
+})
+
+//Adicionar tarefa
+var form_task = document.forms["form-tarefa"];
+form_task.addEventListener("submit", function () {
+    let fname = document.forms["form-tarefa"]["name"].value;
+    let fdate = document.forms["form-tarefa"]["date"].value;
+    let ftime = document.forms["form-tarefa"]["time"].value;
+    let flocal = document.forms["form-tarefa"]["local"].value;
+    let fdesc = document.forms["form-tarefa"]["description"].value;
+    alert(`Nome:${fname} 
+    \nData:${fdate}
+    \nHorario:${ftime}
+    \nLocal:${flocal}
+    \nDescrição${fdesc}`)
+})
