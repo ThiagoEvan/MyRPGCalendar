4f2d0c5cd710a9ca9bd104f52f57d4fdad1e2a1b
ThiagoEvan <Tevangelista504@gmail.com>
2022-11-28 20:16:54 -0300

    Estilização da ficha e iniciando a loja

--- assets/js/inventario.js
+++ assets/js/inventario.js
@@ -1,12 +1,98 @@
-var botaoInv = document.querySelector(".inv")
-var botaoMer = document.querySelector(".mer")
+const items = [
+    {
+        id: 0,
+        nome: "armadura couro",
+        img: "img.jpg",
+        preco: 20,
+        tipo: "roupa",
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: "armadura ouro",
+        img: "img.jpg",
+        tipo: "roupa",
+        preco: 30,
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: "armadura ferro",
+        img: "img.jpg",
+        preco: 25,
+        tipo: "roupa",
+        quantidade: 0
+    },
+    {
+        id: 3,
+        nome: "espada madeira",
+        img: "img.jpg",
+        preco: 20,
+        tipo: "arma",
+        quantidade: 0
+    },
+    {
+        id: 4,
+        nome: "armadura ferro",
+        img: "img.jpg",
+        preco: 30,
+        tipo: "arma",
+        quantidade: 0
+    },
+    {
+        id: 5,
+        nome: "armadura ouro",
+        img: "img.jpg",
+        preco: 25,
+        tipo: "arma",
+        quantidade: 0
+    }
+]
 
-botaoInv.addEventListener("click", function() {
-    document.getElementsByClassName("mercado").style.display = "none";
-    document.getElementsByClassName("inventario").style.display = "flex";
-})
+function InicializarLoja(){
+    var containerMer = document.getElementById('mer');
 
-botaoMer.addEventListener("click", function(){
-    document.getElementsByClassName("inventario").style.display = "none";
-    document.getElementsByClassName("mercado").style.display = "flex"
-})
+    items.map(function(produto){
+        containerMer.innerHTML += ` 
+
+            <div class="produto">
+                <img src="./images/`+produto.img+`"/>
+                <span>`+produto.nome+`</span>
+                <a key="`+produto.id+`" href="#"> Comprar </a> 
+            </div>
+        `
+    }) 
+}
+InicializarLoja();
+
+function atualizarInv(){
+    var containerMer = document.getElementById('inv');
+
+    items.map(function(produto){
+        containerMer.innerHTML += ` 
+
+            <div class="produto">
+                <img src="./images/`+produto.img+`"/>
+                <span>`+produto.nome+`</span>
+                
+            </div>
+        `
+    })
+
+}
+
+var links = document.getElementsByTagName("a")
+
+for (var i = 0; i < links.length; i++) {
+
+    links[i].addEventListener("click", function(){
+    
+        let key = this.getAttribute("key")
+    
+        items[key].quantidade++;
+    
+        atualizarInv();
+    
+        return false;
+    })
+}
