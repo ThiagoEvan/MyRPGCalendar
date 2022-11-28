ce09b927bfbef4ca70518e5103332c4a5f7cdf54
= <=>
2022-11-28 10:58:39 -0300

    Corrigndo agenda e mostrando tarefa na agenda

--- js/index.js
+++ js/index.js
@@ -1,57 +1,81 @@
 var calendarDays = document.getElementById("dias")
 var calendarMonth = document.getElementById("mes")
 var date = new Date()
-var dayNow = date.getDate()
+var year = date.getFullYear()
+
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", 	"December"];
+var month = date.getMonth() + 1
+var nMonth;
+var lMonth;
+if (month == 12){
+	nMonth = 1
+	lMonth = month - 1
+} else if (month == 1) {
+	nMonth = month + 1
+	lMonth = 12
+} else {
+	nMonth = month + 1
+	lMonth = month - 1
+}
+var lastdate = new Date(`${year},${lMonth},01`)
+var nextdate = new Date(`${year},${nMonth},01`)
+
+var week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"] //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sabado
+var thirty_months = ["January","April","June","September","November"]
 
 //Verificar o dia que começa e os dias do mês anterior
-week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"] //Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sabado
-let w = week[date.getDay()]
-var l = date.getDay()
-for(let i = 0; i <= l -1 ; i++){
-    date.setDate(-1)
-    let previous_day = date.getDate()
-    calendarDays.insertAdjacentHTML("afterbegin",`<div class="previous_day">${previous_day+1}</div>`)
+function laterMonth() {
+	date.setDate(0)
+	let w = week[date.getDay()]
+	var l = date.getDay()
+	for(let i = 0; i <= week.indexOf(w); i++){
+    	calendarDays.insertAdjacentHTML("afterbegin",`<div class="previous_day">${date.getDate()-i}</div>`)
+	}
+	reset()
 }
+
 
 //Verificar o limite de dias do Mês atual
-date = new Date()
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var thirty_months = ["January","April","June","September","November"]
-let month = months[date.getMonth()]
-var limite = 31
-for(let i = 0; i <= thirty_months.length - 1; i++){
-    if (month == thirty_months[i]) {
-        limite = 30
-    } else {
-        limite = 28
-    }
+function monthLimit(){
+	nextdate.setDate(0)
+	var limit = nextdate.getDate()
+	
+	reset()
+	return limit
 }
-//Coloca o Mês atual
-calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${month}</h2>`)
+
+function getMonth(){
+	var month = months[date.getMonth()]
+	
+	//Coloca o Mês atual
+	calendarMonth.insertAdjacentHTML("beforeend",`<h2 class="month">${month}</h2>`)
+
+}
+
 
 //Coloca os Dias do Mês
-for(let day = 1; day <= limite; day++){
-    if (day == dayNow) {
-        calendarDays.insertAdjacentHTML("beforeend",`<div class="dayNow">${day}</div>`)
-    } else {
-        calendarDays.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
-    }
+function generateDays(){	
+	var dayNow = date.getDate()
+	var limit = monthLimit()
+	for(let day = 1; day <= limit; day++){
+    	if (day == dayNow) {
+        	calendarDays.insertAdjacentHTML("beforeend",`<div class="dayNow">${day}</div>`)
+    	} else {
+        	calendarDays.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`)
+    	}
+	}
 }
 
-//Verificar os proximos dias do proximo mês (testing...)
-date.setDate(limite+1)
-w = week[date.getDay()]
-for(let i = 0; i <= week.length - 1; i++){
-    if(w == week[i]){
-        limite = 7 - i
-    }
-    
-}
-for(let i = date.getDate(); i <= limite; i++){
-    calendarDays.insertAdjacentHTML("beforeend",`<div class="next_day">${i}</div>`)
-}
 
-//Mostrar tarefea
+//Verificar os proximos dias do proximo mês
+function nextMonth(){
+	let count = 0
+	for(let i = nextdate.getDay(); i <= week.indexOf("Sat") - 1; i++){
+	 	calendarDays.insertAdjacentHTML("beforeend",`<div class="next_day">${count+=1}</div>`)
+	}
+}
+//Mostrar tarefa
 calendarDays.addEventListener("click",function(){
     document
     .getElementById("modal")
@@ -60,16 +84,44 @@ calendarDays.addEventListener("click",function(){
 })
 
 //Adicionar tarefa
-var form_task = document.forms["form-tarefa"];
-form_task.addEventListener("submit", function () {
-    let fname = document.forms["form-tarefa"]["name"].value;
-    let fdate = document.forms["form-tarefa"]["date"].value;
-    let ftime = document.forms["form-tarefa"]["time"].value;
-    let flocal = document.forms["form-tarefa"]["local"].value;
-    let fdesc = document.forms["form-tarefa"]["description"].value;
-    alert(`Nome:${fname} 
-    \nData:${fdate}
-    \nHorario:${ftime}
-    \nLocal:${flocal}
-    \nDescrição${fdesc}`)
+document.querySelector(".save").addEventListener("click", function () {
+    let fname = document.getElementById("name").value
+    let fdate = document.getElementById("date").value
+    let ftime = document.getElementById("time").value
+    let flocal = document.getElementById("local").value
+    let fdesc = document.getElementById("description").value
+	setTask(fdate.slice(8))
+	document.getElementById("modal").classList.toggle("hide")
 })
+
+//cancelar tarefa
+document.querySelector(".cancel").addEventListener("click",function() {
+	    document
+    .getElementById("modal")
+    .classList
+    .toggle("hide")
+})
+
+//Colocar tarefa no calendario
+function setTask(day){
+	var days = document.querySelectorAll("div.day")
+	if (day < 10){
+		day = day.toString().slice(1)
+	}
+	for(var i = 0;i <= days.length - 1;i++){
+		if(day.toString() == days[i].innerHTML){
+			days[i].style.backgroundColor = "cyan"
+		}
+	}
+}
+
+function init() {
+	getMonth()
+	laterMonth()
+	generateDays()
+	nextMonth()
+}
+function reset() {
+	date = new Date()
+}
+init()
