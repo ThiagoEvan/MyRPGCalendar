a9d720903e29c3dffb6079bf3029d16b258f22be
DiogoWorkGit <diogowork00@gmail.com>
2022-11-28 18:01:15 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,65 @@
+var arr = [];
+var brr = [];
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function toglepop() {
+    var x = document.getElementsByClassName("container-pop");
+    x = x[0];
+    var y = document.getElementsByClassName("container");
+    y = y[0]; 
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "flex";
+    }
+}
+
+function storeAccount(){
+    var x = document.getElementsByClassName("container-pop");
+    x = x[0];
+    var y = document.getElementsByClassName("container");
+    y = y[0];
+
+    let usuario = document.querySelectorAll("input")[2].value;
+    let email = document.querySelectorAll("input")[3].value;
+    let senha = document.querySelectorAll("input")[4].value;
+    
+    console.log(usuario, email, senha);
+
+    document.querySelectorAll("input")[2].value = "";
+    document.querySelectorAll("input")[3].value = "";
+    document.querySelectorAll("input")[4].value = "";
+
+    localStorage.setItem(usuario, senha)
+    x.style.display = "none";
+    y.style.display = "flex";
+}
+
+function logar(){
+    let usuario = document.querySelectorAll("input")[0].value;
+    let senha = document.querySelectorAll("input")[1].value;
+
+    console.log(usuario, senha);
+
+    let autenticar = localStorage.getItem(usuario)
+
+    if (autenticar === senha){
+        console.log("logado")
+        document.querySelectorAll("input")[0].value = "";
+        document.querySelectorAll("input")[1].value = "";
+        document.getElementById("confirma-login").innerHTML = "Logado com sucesso!"
+        sleep(100)
+        window.location.href = "./index.html";
+    } else {
+        document.querySelectorAll("input")[0].value = "";
+        document.querySelectorAll("input")[1].value = "";
+        document.getElementById("confirma-login").innerHTML = "Senha ou usuário errado!"
+    }
+    
+
+}
