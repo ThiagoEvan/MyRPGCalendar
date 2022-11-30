b80fda300c4ae0656c69d14d3f34821a0c380070
DiogoWorkGit <diogowork00@gmail.com>
2022-11-30 11:36:56 -0300

    arrumado alguns bugs do login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,3 @@
-var arr = [];
-var brr = [];
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -13,9 +10,11 @@ function toglepop() {
     if (x.style.display === "none") {
         x.style.display = "flex";
         y.style.display = "none";
+        document.getElementById("confirma-login").innerHTML = ""
     } else {
         x.style.display = "none";
         y.style.display = "flex";
+        document.getElementById("confirma-login").innerHTML = ""
     }
 }
 
@@ -26,18 +25,25 @@ function storeAccount(){
     y = y[0];
 
     let usuario = document.querySelectorAll("input")[2].value;
+    let arr = [];
     let email = document.querySelectorAll("input")[3].value;
     let senha = document.querySelectorAll("input")[4].value;
     if (usuario !== null && senha !== null && email !== null && usuario !== "" && senha !== "" && email !== ""){
-        console.log(usuario, email, senha);
+        if (localStorage.getItem(usuario) === null){
+            console.log(usuario, email, senha);
 
-        document.querySelectorAll("input")[2].value = "";
-        document.querySelectorAll("input")[3].value = "";
-        document.querySelectorAll("input")[4].value = "";
-
-        localStorage.setItem(usuario, senha);
-        x.style.display = "none";
-        y.style.display = "flex";
+            document.querySelectorAll("input")[2].value = "";
+            document.querySelectorAll("input")[3].value = "";
+            document.querySelectorAll("input")[4].value = "";
+    
+            arr.push(email);
+            arr.push(senha);
+    
+            localStorage.setItem(usuario, JSON.stringify(arr));
+            x.style.display = "none";
+            y.style.display = "flex";
+        }
+        document.getElementById("confirma-registro").innerHTML = "Esse nome de usuário já está em uso!!"
     }
 }
 
@@ -48,13 +54,15 @@ function logar(){
     console.log(usuario, senha);
 
     let autenticar = localStorage.getItem(usuario)
-
+    autenticar = autenticar.replaceAll("[","").replaceAll("]","").replaceAll('"','').split(",")
+    autenticar = autenticar[1]
+    console.log(autenticar[1]);
     if (autenticar === senha){
         console.log("logado")
         document.querySelectorAll("input")[0].value = "";
         document.querySelectorAll("input")[1].value = "";
         document.getElementById("confirma-login").innerHTML = "Logado com sucesso!"
-        sleep(100)
+        sleep(200)
         window.location.href = "./index.html";
     } else {
         document.querySelectorAll("input")[0].value = "";
